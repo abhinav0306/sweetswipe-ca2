212ed1a64580f468476bcee4c453f94b6662499e
abhinav0306 <abhinavs0603@gmail.com>
2023-11-21 11:02:15 +0530

    commit1

---
+++ app.js
@@ -0,0 +1,5 @@
+//adding functionality to play button and redirecting it to next page
+var playButton = document.getElementById('playbtn');
+playButton.addEventListener('click', function() {
+    window.location.href = 'credentials.html';
+});
---
+++ credentials.js
@@ -0,0 +1,5 @@
+//adding functionality to start button and redirecting it to next page
+var startBtn = document.getElementById('start');
+startBtn.addEventListener('click', function() {
+    window.location.href = 'instruction.html';
+});
