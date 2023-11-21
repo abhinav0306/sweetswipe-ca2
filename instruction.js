fe0ad6ceb2f8402df060cb04f7a88cd1039fddee
abhinav0306 <abhinavs0603@gmail.com>
2023-11-21 13:04:38 +0530

    commit3

---
+++ instruction.js
@@ -0,0 +1,5 @@
+//adding functionality to let's crush and redirecting it to next page
+var crushBtn = document.getElementById('crush');
+crushBtn.addEventListener('click', function() {
+    window.location.href = 'game.html';
+});
