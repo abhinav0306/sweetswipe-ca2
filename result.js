a07152dc8c8a4e5388d15571ba4fda47b88d7b1e
abhinav0306 <abhinavs0603@gmail.com>
2023-11-21 14:14:26 +0530

    commit4

---
+++ result.js
@@ -0,0 +1,5 @@
+//adding functionality to restart button and redirecting it to next page
+var restartBtn = document.getElementById('restart');
+restartBtn.addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
